360de68b79e6e2af645807a62448f2a807285fca
Ben Codrington <bcodrington@uncharted.software>
2021-05-27 09:42:17 -0400

    Fix pickling errors - fix missing INDRA_HOST parameter - remove old print() call

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-### Anansi
+## Anansi
 Anansi is the WM knowledge data ingestion pipelines. It provides a suite of tools for dealing with and transforming INDRA statement and DART CDRs.
 
-#### Prereq
+### Prereq
 We assume the geolocation reference dataset is in place and ready to be used. To install the geolocation data please see TODO
 
-#### Ussage scenarios
+### Usage scenarios
 There are three separate scenarios where we would need to ingest knowledge
 - Adhoc ingestion, for internal testing or one-off requests
 - An initial data ingestion when the corpus is first released to the wild
@@ -16,3 +16,34 @@ Despite these different cases, all knowledge ingestion will more or less follow 
 - Collect new statements
 - For each statement, transfrom inject document context and geolocation context
 - Index into ElasticSearch
+
+
+### Prefect Background and Commands
+
+The `incremental_pipeline.py` file is decorated and structured to be runnable through our [Prefect server](http://10.65.18.52:8080/default).
+
+There is a [Prefect agent](https://docs.prefect.io/orchestration/agents/overview.html) running on that machine, labelled with `non-dask`. Flows with the same tag will be run by this agent instead of a Dask agent that will attempt to run some tasks in parallel.
+
+Code changes currently need to be manually copied over, including local dependencies like `dart`, `elastic`, etc. GitLab is run on `10.64` and therefore we can't `git clone` from it on the `10.65` machine.
+
+TODO: Set up a script to copy the files over and fetch any dependencies or look into automating this from GitLab directly when code is merged in.
+
+After SSHing into `10.65.18.52`:
+
+#### To rerun the agent:
+
+- `tmux a -t seq-agent`
+- CTRL+C to cancel it
+- `source flows/.incremental_assembly_secrets.sh` to learn any secrets that have changed
+- `PREFECT__ENGINE__EXECUTOR__DEFAULT_CLASS="prefect.executors.LocalExecutor" PYTHONPATH="${PYTHONPATH}:/home/centos/flows" prefect agent local start --api "http://10.65.18.52:4200/graphql" --label "non-dask"` to run the non-dask agent
+- (CTRL+B D to exit the tmux session)
+
+#### To run/register the flow:
+
+- Set the `shouldRegister` flag in the python file
+- `conda activate prefect-seq` to switch to a python env with the correct dependencies
+- `python incremental_assembly.py` to run the file and either run the flow locally or register it with the prefect server
+
+#### To create the agent in the first place:
+
+`conda create -n prefect-seq -c conda-forge "python>=3.8.0" prefect "elasticsearch==7.11.0" "boto3==1.17.18" "smart_open==5.0.0" python-dateutil requests`
--- src/incremental_pipeline.py
+++ src/incremental_pipeline.py
@@ -1,160 +1,237 @@
-import logging
-import json
-import os
-from smart_open import open
-from elastic import Elastic
-from requests.auth import HTTPBasicAuth
-from dart import document_transform, get_CDRs
-import requests
-
-FORMAT = "%(asctime)-25s %(levelname)-8s %(message)s"
-logging.basicConfig(format=FORMAT, level=logging.INFO)
-logger = logging.getLogger(__name__)
-
+from prefect import task, Flow, Parameter
+from prefect.run_configs import LocalRun
+from prefect.executors import LocalExecutor
+from typing import Tuple
 from indra import influence_transform, metadata_transfrom, evidence_transform, get_wm, IndraAPI
-from utils import json_file_content
+import os
+from elastic import Elastic
+from dart import document_transform, get_CDRs
 
 
-# Environment
-INDRA_HOST = os.environ.get("INDRA_HOST") # "http://wm.indra.bio/"
+@task(log_stdout=True)
+def read_environment_variables() -> Tuple[str, str, str, str, str, str, str, str]:
+    # Environment
+    INDRA_HOST = os.environ.get("INDRA_HOST")  # "http://wm.indra.bio/"
 
-# Resources like geolocation and corpus, generally speaking source/target will be the same
-SOURCE_ES_HOST = os.environ.get("SOURCE_ES_HOST")
-SOURCE_ES_PORT = os.environ.get("SOURCE_ES_PORT")
+    # Resources like geolocation and corpus, generally speaking source/target will be the same
+    SOURCE_ES_HOST = os.environ.get("SOURCE_ES_HOST")
+    SOURCE_ES_PORT = os.environ.get("SOURCE_ES_PORT")
 
-# Where to index documents
-TARGET_ES_HOST = os.environ.get("TARGET_ES_HOST")
-TARGET_ES_PORT = os.environ.get("TARGET_ES_PORT")
+    # Where to index documents
+    TARGET_ES_HOST = os.environ.get("TARGET_ES_HOST")
+    TARGET_ES_PORT = os.environ.get("TARGET_ES_PORT")
+    DART_HOST = os.environ.get("DART_HOST")
+    DART_USER = os.environ.get("DART_USER")
+    DART_PASS = os.environ.get("DART_PASS")
 
-DART_HOST = os.environ.get("DART_HOST");
-DART_USER = os.environ.get("DART_USER");
-DART_PASS= os.environ.get("DART_PASS");
-
-# Request
-PROJECT_EXTENSION_ID = os.environ.get("PROJECT_EXTENSION_ID")
-
-
-# Fake payload
-FAKE_INDRA_REQUEST = {
-  "id": "xyz",
-  "project_id": "project-fd729e8d-6686-4761-ab7d-2eb906044a65",
-  "records": [
-      {
-          "document_id": "570bc75dba3cfb995445932c57329775",
-          "identity": "eidos",
-          "storage_key": "a5ff1d64-1371-4edd-ad3e-47d8c899e6bc.jsonld",
-          "version": "1.1.0"
-      }
-  ]
-}
-
-# Vars
-source_es = Elastic(SOURCE_ES_HOST, SOURCE_ES_PORT)
-target_es = Elastic(TARGET_ES_HOST, TARGET_ES_PORT)
+    return (
+        INDRA_HOST,
+        SOURCE_ES_HOST,
+        SOURCE_ES_PORT,
+        TARGET_ES_HOST,
+        TARGET_ES_PORT,
+        DART_HOST,
+        DART_USER,
+        DART_PASS
+    )
 
 
-# 0. Print out input constants
-logger.info(f"Source Elastic: {SOURCE_ES_HOST}:{SOURCE_ES_PORT}")
-logger.info(f"Target Elastic: {TARGET_ES_HOST}:{TARGET_ES_PORT}")
-logger.info(f"INDRA: {INDRA_HOST}")
-logger.info(f"PROJECT_EXTENSION_ID: {PROJECT_EXTENSION_ID}")
+@task(log_stdout=True)
+def print_inputs(
+    SOURCE_ES_HOST,
+    SOURCE_ES_PORT,
+    TARGET_ES_HOST,
+    TARGET_ES_PORT,
+    INDRA_HOST,
+    PROJECT_EXTENSION_ID
+):
+    # 0. Print out input constants
+    print(f"Source Elastic: {SOURCE_ES_HOST}:{SOURCE_ES_PORT}")
+    print(f"Target Elastic: {TARGET_ES_HOST}:{TARGET_ES_PORT}")
+    print(f"INDRA: {INDRA_HOST}")
+    print(f"PROJECT_EXTENSION_ID: {PROJECT_EXTENSION_ID}")
 
-# 1. Fetch project-extension from source-es by id
-logger.info("Fetching project-extension")
-
-extension = source_es.term_query("project-extension", "_id", PROJECT_EXTENSION_ID)
-# FIXME - temporary test
-extension = FAKE_INDRA_REQUEST
-
-# 2. Extract relevant fields from project-extension document
-project_id = extension["project_id"]
-records = extension["records"]
-logger.info(f"Found extension with {len(records)} records for project: {project_id}")
+    if (SOURCE_ES_HOST is None or
+            SOURCE_ES_PORT is None or
+            TARGET_ES_HOST is None or
+            TARGET_ES_PORT is None or
+            INDRA_HOST is None):
+        raise ValueError(
+            "Missing required environment variables. Ensure that you've run `source ./.incremental_assembly_secrets.sh`.")
 
 
-# 3. Process CDR
-doc_ids = []
-for record in records:
-    doc_ids.append(record["document_id"])
-doc_ids = list(set(doc_ids))
+@task(log_stdout=True)
+def fetch_project_extension(project_extension_id, SOURCE_ES_HOST, SOURCE_ES_PORT) -> Tuple[str, list]:
+    source_es = Elastic(SOURCE_ES_HOST, SOURCE_ES_PORT)
+    # 1. Fetch project-extension from source-es by id
+    print("Fetching project-extension")
+    extension = source_es.term_query(
+        "project-extension", "_id", project_extension_id)
+    project_id = extension["project_id"]
+    records = extension["records"]
+    print(
+        f"Found extension with {len(records)} records for project: {project_extension_id}")
+    return (project_id, records)
 
-cdrs = get_CDRs(DART_HOST, DART_USER, DART_PASS, doc_ids)
 
-counter = 0
-es_buffer = []
-for cdr in cdrs:
-    es_buffer.append(document_transform(cdr))
-    counter = counter + 1
-    if counter % 500 == 0:
-        logger.info(f"\tIndexing ... {counter}")
+@task(log_stdout=True)
+def process_cdrs(records, DART_HOST, DART_USER, DART_PASS, TARGET_ES_HOST, TARGET_ES_PORT):
+
+    target_es = Elastic(TARGET_ES_HOST, TARGET_ES_PORT)
+    # 3. Process CDR
+    doc_ids = []
+    for record in records:
+        doc_ids.append(record["document_id"])
+    doc_ids = list(set(doc_ids))
+
+    cdrs = get_CDRs(DART_HOST, DART_USER, DART_PASS, doc_ids)
+    counter = 0
+    es_buffer = []
+    for cdr in cdrs:
+        es_buffer.append(document_transform(cdr))
+        counter = counter + 1
+        if counter % 500 == 0:
+            print(f"\tIndexing ... {counter}")
+            target_es.bulk_write('corpus', es_buffer)
+            es_buffer = []
+    if len(es_buffer) > 0:
         target_es.bulk_write('corpus', es_buffer)
 
-if len(es_buffer) > 0:
-    target_es.bulk_write('corpus', es_buffer)
+
+@task(log_stdout=True)
+def request_reassembly(project_id, records, INDRA_HOST):
+    # 4. Send request to INDRA for reassembly
+    print("Sending project-extension request to INDRA for reassembly")
+    indra = IndraAPI(INDRA_HOST)
+    response = indra.add_project_records(project_id, records)
+    return response
 
 
-# 4. Send request to INDRA for reassembly
-logger.info("Sending project-extension request to INDRA for reassembly")
-indra = IndraAPI(INDRA_HOST)
-response = indra.add_project_records(project_id, records)
-
-# 5. Parse INDRA response
-new_stmts = response["new_stmts"]
-new_evidence = response["new_evidence"]
-new_refinements = response["new_refinements"]
-beliefs = response["beliefs"]
-logger.info("=" * 50)
-logger.info(f"{len(new_stmts)} new statements.")
-logger.info(f"{len(new_evidence)} new pieces of evidence.")
-logger.info(f"{len(new_refinements)} new refinements.")
-logger.info(f"{len(beliefs)} belief scores.")
-logger.info("=" * 50)
-
-# new_stmts = {} # Testing
-
-# 6. Pivot "new_stmts" into array of INDRA statements and join with "beliefs", transform and index new statements to project index
-logger.info("")
-logger.info("Processing new statements")
-counter = 0
-es_buffer = []
-for statement in new_stmts.values():
-  matches_hash = str(statement["matches_hash"])
-  statement["evidence"] = new_evidence.get(matches_hash)
-  statement["belief"] = beliefs.get(matches_hash)
-  result = influence_transform(statement, source_es)
-  counter = counter + 1
-  es_buffer.append(result)
-  if counter % 500 == 0:
-      logger.info(f"\tIndexing ... {counter}")
-      target_es.bulk_write(project_id, es_buffer)
-      es_buffer = []
-
-if len(es_buffer) > 0:
-    logger.info(f"\tIndexing ... {counter}")
-    target_es.bulk_write(project_id, es_buffer)
+@task(log_stdout=True)
+def apply_reassembly_to_es(
+    response,
+    project_id,
+    SOURCE_ES_HOST,
+    SOURCE_ES_PORT,
+    TARGET_ES_HOST,
+    TARGET_ES_PORT
+):
+    source_es = Elastic(SOURCE_ES_HOST, SOURCE_ES_PORT)
+    target_es = Elastic(TARGET_ES_HOST, TARGET_ES_PORT)
+    # 5. Parse INDRA response
+    new_stmts = response["new_stmts"]
+    new_evidence = response["new_evidence"]
+    new_refinements = response["new_refinements"]
+    beliefs = response["beliefs"]
+    print("=" * 50)
+    print(f"{len(new_stmts)} new statements.")
+    print(f"{len(new_evidence)} new pieces of evidence.")
+    print(f"{len(new_refinements)} new refinements.")
+    print(f"{len(beliefs)} new belief scores.")
+    print("=" * 50)
+    # 6. Pivot "new_stmts" into array of INDRA statements and join with "beliefs",
+    #   transform and index new statements to project index
+    counter = 0
     es_buffer = []
+    for statement in new_stmts.values():
+        matches_hash = str(statement["matches_hash"])
+        statement["evidence"] = new_evidence.get(matches_hash)
+        statement["belief"] = beliefs.get(matches_hash)
+        result = influence_transform(statement, source_es)
+        counter = counter + 1
+        es_buffer.append(result)
+        if counter % 500 == 0:
+            print(f"\tIndexing ... {counter}")
+            target_es.bulk_write(project_id, es_buffer)
+            es_buffer = []
+    if len(es_buffer) > 0:
+        print(f"\tIndexing ... {counter}")
+        target_es.bulk_write(project_id, es_buffer)
+        es_buffer = []
+    # 7. Merge new evidence
+    # Not very efficient, should do batch queries and partial fetches + update
+    print("")
+    print("Processing updated statements")
+    update_buffer = []
+    for key, evidence in new_evidence.items():
+        if key not in new_stmts:
+            stmt = source_es.term_query(project_id, "matches_hash", key)
+            if stmt == None:
+                continue
 
-# 7 Merge new evidence 
-# Not very efficient, should do batch queries and partial fetches + update
-logger.info("")
-logger.info("Processing updated statements")
-update_buffer = []
-for key, evidence in new_evidence.items():
-    if key not in new_stmts: 
-        stmt = source_es.term_query(project_id, "matches_hash", key)
-        if stmt == None:
-            continue
+            for ev in evidence:
+                stmt["evidence"].append(evidence_transform(ev, source_es))
 
-        for ev in evidence:
-            stmt["evidence"].append(evidence_transform(ev, source_es))
-
-        stmt["wm"] = get_wm(stmt["evidence"], stmt["subj"], stmt["obj"])
-        update_buffer.append(stmt)
-
-if len(update_buffer) > 0:
-    logger.info(f"\tIndexing ... {len(update_buffer)}")
-    target_es.bulk_write(project_id, update_buffer)
+            stmt["wm"] = get_wm(stmt["evidence"], stmt["subj"], stmt["obj"])
+            update_buffer.append(stmt)
+    if len(update_buffer) > 0:
+        print(f"\tIndexing ... {len(update_buffer)}")
+        target_es.bulk_write(project_id, update_buffer)
 
 
-# 8. Mark as completed ??
-logger.info(f"Updated statements for project {project_id}.")
+@task(log_stdout=True)
+def mark_completed(project_id):
+    print(f"Updated statements for project {project_id}.")
+
+
+with Flow("incremental assembly", run_config=LocalRun(labels=["non-dask"])) as flow:
+    PROJECT_EXTENSION_ID = Parameter(
+        "PROJECT_EXTENSION_ID", default="ae814f9e-b9b3-46ca-b310-53f83fa1ec76")
+    (
+        INDRA_HOST,
+        SOURCE_ES_HOST,
+        SOURCE_ES_PORT,
+        TARGET_ES_HOST,
+        TARGET_ES_PORT,
+        DART_HOST,
+        DART_USER,
+        DART_PASS
+    ) = read_environment_variables()
+    print_inputs(
+        SOURCE_ES_HOST,
+        SOURCE_ES_PORT,
+        TARGET_ES_HOST,
+        TARGET_ES_PORT,
+        INDRA_HOST,
+        PROJECT_EXTENSION_ID
+    )
+    (project_id, records) = fetch_project_extension(
+        PROJECT_EXTENSION_ID, SOURCE_ES_HOST, SOURCE_ES_PORT)
+    process_cdrs(
+        records,
+        DART_HOST,
+        DART_USER,
+        DART_PASS,
+        TARGET_ES_HOST,
+        TARGET_ES_PORT
+    )
+    response = request_reassembly(project_id, records, INDRA_HOST)
+    apply_reassembly_to_es(
+        response,
+        project_id,
+        SOURCE_ES_HOST,
+        SOURCE_ES_PORT,
+        TARGET_ES_HOST,
+        TARGET_ES_PORT
+    )
+    mark_completed(project_id)
+
+# ===========================================================
+
+# Set this to False to run the flow locally, or
+# set it to True to register the flow with the Prefect server
+# to be run later on an agent.
+should_register = True
+
+
+if (should_register):
+    flow.register(project_name="project")
+else:
+    state = flow.run(
+        executor=LocalExecutor(),
+        parameters={
+            "PROJECT_EXTENSION_ID": "ae814f9e-b9b3-46ca-b310-53f83fa1ec76"
+        }
+    )
+
+    assert state.is_successful()
