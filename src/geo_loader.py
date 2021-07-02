18d13533d40b7bbfcbf7fde2ea36dec2fd0a6df2
Daniel Chang <dchang@uncharted.software>
2021-07-02 10:27:09 -0400

    consistent quotes

--- src/geo_loader.py
+++ src/geo_loader.py
@@ -4,7 +4,7 @@ import numpy as np
 from elastic import Elastic
 
 
-'''
+"""
 This script is meant to update the geo index.
 The data comes from the following URLs:
 http://download.geonames.org/export/dump/allCountries.zip
@@ -14,11 +14,11 @@ Those files are needed locally in the root folder
 
 It keeps the geo_id, name, lat and lon columns, as well as filter out rows where the
 feature column contains U, T, R, L, H, S, V.
-'''
+"""
 
 
 def filter_rows_and_columns(df, columns):
-    return df[~df['feature'].isin(["U", "T", "R", "L", "H", "S", "V"])][columns]
+    return df[~df["feature"].isin(["U", "T", "R", "L", "H", "S", "V"])][columns]
 
 
 def format_document(df, columns):
@@ -27,30 +27,30 @@ def format_document(df, columns):
 root = os.getcwd()
 ES_url = os.environ.get("ES_url")
 
-all_countries_df = pd.read_csv(root + '/allCountries.txt', delimiter = "\t", header=None)
-all_countries_df.columns = ['geo_id', 'name', 'asciiname', 'alternatenames', 'lat', 'lon', 'feature',
- 'feature code', 'country code', 'cc2', 'admin1 code', 'admin2 code', 'admin3 code', 'admin4 code',
-  'population', 'elevation', 'dem', 'timezone', 'modification date']
+all_countries_df = pd.read_csv(root + "/allCountries.txt", delimiter = "\t", header=None)
+all_countries_df.columns = ["geo_id", "name", "asciiname", "alternatenames", "lat", "lon", "feature",
+ "feature code", "country code", "cc2", "admin1 code", "admin2 code", "admin3 code", "admin4 code",
+  "population", "elevation", "dem", "timezone", "modification date"]
 
 
-relevant_cols = ['geo_id', 'name', 'lat', 'lon']
+relevant_cols = ["geo_id", "name", "lat", "lon"]
 all_countries_df = filter_rows_and_columns(all_countries_df, relevant_cols)
 
 es = Elastic(ES_url)
 
 es_documents = format_document(all_countries_df, relevant_cols)
 
-es.bulk_write('geo', es_documents)
+es.bulk_write("geo", es_documents)
 
 all_countries_df = None
 
-gadm_woredas_df = pd.read_csv(root + '/gadm_woredas.txt', delimiter = "\t", header=None)
-gadm_woredas_df.columns = ['geo_id', 'name', 'asciiname', 'alternatenames', 'lat', 'lon', 'feature',
- 'feature code', 'country code', 'cc2', 'admin1 code', 'admin2 code', 'admin3 code', 'admin4 code',
-  'population', 'elevation', 'dem', 'timezone', 'modification date']
+gadm_woredas_df = pd.read_csv(root + "/gadm_woredas.txt", delimiter = "\t", header=None)
+gadm_woredas_df.columns = ["geo_id", "name", "asciiname", "alternatenames", "lat", "lon", "feature",
+ "feature code", "country code", "cc2", "admin1 code", "admin2 code", "admin3 code", "admin4 code",
+  "population", "elevation", "dem", "timezone", "modification date"]
 
 gadm_woredas_df = filter_rows_and_columns(gadm_woredas_df, relevant_cols)
 
 es_documents = format_document(gadm_woredas_df, relevant_cols)
 
-es.bulk_write('geo', es_documents)
+es.bulk_write("geo", es_documents)
