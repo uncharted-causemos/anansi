8939926a548a76e7b5b066547d13f770d1de0c76
dchang <dchang@uncharted.software>
2022-06-24 09:03:57 -0400

    parameterize dart cdr url

--- src/web/dart.sh
+++ src/web/dart.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash 
+#!/usr/bin/env sh
 
 # Clean up
 echo "Starting DART CDR extraction"
@@ -7,7 +7,7 @@ rm -rf ${NAME}-dart_cdr
 rm -rf ${WATCH_FOLDER}/${NAME}-dart_cdr.json
 
 # curl -XGET -H "Accept: application/zip" "http://localhost/dart/api/v1/cdrs/archive" -o ${NAME}-dart.zip
-curl -XGET -H "Accept: application/zip" "http://10.64.16.209:4005/dart-may-2021/auto-test.zip" -o ${NAME}-dart.zip
+curl -XGET -H "Accept: application/zip" "${DART_CDR_URL}" -o ${NAME}-dart.zip
 unzip -j ${NAME}-dart.zip -d ${NAME}-dart_cdr 
 
 for f in ${NAME}-dart_cdr/*.json; do
--- src/web/index.js
+++ src/web/index.js
@@ -77,8 +77,15 @@ app.get('/status/:id', asyncHandler(async (req, res) => {
 }));
 
 
+/**
+ * Check a directory/folder at regular intervals. This is contorlled by three env variables
+ * - WATCH_FOLDER 
+ * - WATCH_INTERVAL
+ * - DART_CDR_URL
+*/
 const WATCH_FOLDER = process.env.WATCH_FOLDER;
-const WATCH_INTERVAL = process.env.WATCH_INTERVAL || 30000;
+const WATCH_INTERVAL = +(process.env.WATCH_INTERVAL) || 30000;
+const DART_CDR_URL = process.env.DART_CDR_URL || 'http://10.64.16.209:4005/dart-may-2021/auto-test.zip';
 
 if (WATCH_FOLDER) {
   console.log(`Watching ${WATCH_FOLDER}`);
@@ -103,7 +110,7 @@ if (WATCH_FOLDER) {
       console.log(`Processing new data set ${indra}`);
 
       // Download a dart CDR set per indra
-      exec(`WATCH_FOLDER=${WATCH_FOLDER} NAME=${indra} ./dart.sh`, (errDart, stdoutDart, stderrDart) => {
+      exec(`DART_CDR_URL=${DART_CDR_URL} WATCH_FOLDER=${WATCH_FOLDER} NAME=${indra} ./dart.sh`, (errDart, stdoutDart, stderrDart) => {
         console.log(errDart);
         console.log(stdoutDart);
         console.log(stderrDart);
