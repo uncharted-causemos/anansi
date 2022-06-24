595838e50af14725dc176636a2996fc637c2ebda
dchang <dchang@uncharted.software>
2022-06-24 09:18:35 -0400

    add auth

--- src/web/dart.sh
+++ src/web/dart.sh
@@ -6,8 +6,14 @@ rm -f ${NAME}-dart.zip
 rm -rf ${NAME}-dart_cdr
 rm -rf ${WATCH_FOLDER}/${NAME}-dart_cdr.json
 
-# curl -XGET -H "Accept: application/zip" "http://localhost/dart/api/v1/cdrs/archive" -o ${NAME}-dart.zip
-curl -XGET -H "Accept: application/zip" "${DART_CDR_URL}" -o ${NAME}-dart.zip
+
+AUTH=`echo -n ${DART_USERNAME}:${DART_PASSWORD} | base64`
+
+curl -XGET \
+  -H "Authorization: Basic $AUTH" \
+  -H "Accept: application/zip" \
+  "${DART_CDR_URL}" -o ${NAME}-dart.zip
+
 unzip -j ${NAME}-dart.zip -d ${NAME}-dart_cdr 
 
 for f in ${NAME}-dart_cdr/*.json; do
